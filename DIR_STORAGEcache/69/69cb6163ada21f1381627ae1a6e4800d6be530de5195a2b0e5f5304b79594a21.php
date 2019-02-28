5ea62e4cb75efc11611259c60ee83d3b902441ae
Isaac <ikiptanui@powergen-re.com>
2019-02-28 09:45:57 +0300

    first commit

---
+++ DIR_STORAGEcache/69/69cb6163ada21f1381627ae1a6e4800d6be530de5195a2b0e5f5304b79594a21.php
@@ -0,0 +1,49 @@
+<?php
+
+/* default/template/common/content_bottom.twig */
+class __TwigTemplate_4174608ab50e911b77a4ed19569b5109e66bbe74962c6701e2b3eeeace27feb0 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["modules"]) ? $context["modules"] : null));
+        foreach ($context['_seq'] as $context["_key"] => $context["module"]) {
+            // line 2
+            echo $context["module"];
+            echo "
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['module'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "default/template/common/content_bottom.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  23 => 2,  19 => 1,);
+    }
+}
+/* {% for module in modules %}*/
+/* {{ module }}*/
+/* {% endfor %}*/
---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/glorisa/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/glorisa/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/GLORISA/upload/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/GLORISA/upload/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/GLORISA/upload/image/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_STORAGE', 'C:/xampp/storage/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'glorisa');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
